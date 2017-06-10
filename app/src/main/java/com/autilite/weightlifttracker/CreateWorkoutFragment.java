41b1da9794f5228dea5ce147ae2d0d640064fc6b
autilite <autilite@gmail.com>
2017-06-10 01:44:43 -0700

    Create UI element to add new exercise workout row

--- app/src/main/java/com/autilite/weightlifttracker/CreateWorkoutFragment.java
+++ app/src/main/java/com/autilite/weightlifttracker/CreateWorkoutFragment.java
@@ -6,6 +6,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 /**
  * Created by Kelvin on Jun 9, 2017.
@@ -35,12 +41,13 @@ public class CreateWorkoutFragment extends DialogFragment {
         }
     }
 
-
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = View.inflate(getActivity(), R.layout.dialog_create_workout, null);
+
         builder.setTitle(R.string.create_workout_title)
-                .setView(R.layout.dialog_create_workout)
+                .setView(view)
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -52,8 +59,21 @@ public class CreateWorkoutFragment extends DialogFragment {
                     public void onClick(DialogInterface dialogInterface, int i) {
                         mListener.onDialogNegativeClick(CreateWorkoutFragment.this);
                     }
-                })
-        ;
-        return builder.create();
+                });
+
+        Dialog d = builder.create();
+
+        final TableLayout table = (TableLayout) view.findViewById(R.id.workout_create_table);
+        Button button = (Button) view.findViewById(R.id.button_add_exercise);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = LayoutInflater.from(getActivity());
+                TableRow row = (TableRow) inflater.inflate(R.layout.workout_table_row, table,false);
+                table.addView(row);
+            }
+        });
+        return d;
     }
+
 }
