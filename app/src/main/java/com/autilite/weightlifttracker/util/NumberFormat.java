5cbc8b95c10d1ef2b69f61e74110b0b5e73ccc91
autilite <autilite@gmail.com>
2017-07-26 16:42:27 -0700

    Number parser utility

---
+++ app/src/main/java/com/autilite/weightlifttracker/util/NumberFormat.java
@@ -0,0 +1,31 @@
+package com.autilite.weightlifttracker.util;
+
+/**
+ * Created by Kelvin on Jul 26, 2017.
+ */
+
+public class NumberFormat {
+    public static int parseInt(String value, int defaultValue) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    public static float parseFloat(String value, float defaultValue) {
+        try {
+            return Float.parseFloat(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    public static double parseDouble(String value, double defaultValue) {
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+}
